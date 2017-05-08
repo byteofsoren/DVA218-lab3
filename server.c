d50a8d6685296a672e182b9f973fffaf44d98230
wicktor <wlw14001@student.mdh.se>
2017-05-08 11:19:54 +0200

    Commenting

--- server.c
+++ server.c
@@ -33,8 +33,8 @@ int make_Socket4(unsigned short int port) {
     int sock;
     struct sockaddr_in name;
 
-    /* Create a socket. */
-    sock = socket(PF_INET, SOCK_DGRAM, 0);     //TCP connection
+    /* Create a socket. TCP connection */
+    sock = socket(PF_INET, SOCK_DGRAM, 0);
     if(sock < 0) {
         perror("Could not create a socket\n");
         exit(EXIT_FAILURE);
@@ -51,8 +51,11 @@ int make_Socket4(unsigned short int port) {
     }
     return(sock);
 }
-
-
+/* Threeway - This is the server part of the threeway handshake
+ * Inputs:
+ * fileDescriptor - Socket handle
+ * activeFdSet - List of active FDs (which is only one, port 5555)
+ * hostInfo - struct for handling internet addresses */
 void Threeway(int *fileDescriptor, fd_set *activeFdSet, struct sockaddr_in *hostInfo) {
 
     printf("Hej nu är det dags för connect!\n");
@@ -70,14 +73,15 @@ void Threeway(int *fileDescriptor, fd_set *activeFdSet, struct sockaddr_in *host
             /* case 0 - Waiting for SYN from client */
             case 0:
                 readFdSet = *activeFdSet;
-
+                /* Looking for changes in FD */
                 if(select(FD_SETSIZE, &readFdSet, NULL, NULL, NULL) < 0)
                     perror("Server - Select failure");
-
+                /*  */
                 if(FD_ISSET(*fileDescriptor, &readFdSet))
                 {
+                    /* Reads the package from client */
                     ingsoc_readMessage(*fileDescriptor, &toRead, hostInfo);
-
+                    /* If it receives the SYN it proceeds to the next state */
                     if(toRead.SYN == true)
                     {
                         printf("Server - SYN received\n");
@@ -96,23 +100,29 @@ void Threeway(int *fileDescriptor, fd_set *activeFdSet, struct sockaddr_in *host
 
 
                 do {
+                    /* Sends the SYN+ACK package to client */
                     ingsoc_writeMessage(*fileDescriptor, &toWrite, sizeof(toWrite), hostInfo);
                     printf("Server - ACK + SYN sent\n");
                     timer.tv_sec = 20;
                     readFdSet = *activeFdSet;
+                    /* Looks for changes in FD */
                     if(select(FD_SETSIZE, &readFdSet, NULL, NULL, &timer) < 0)
                         perror("Server - Select failure");
 
                     if(FD_ISSET(*fileDescriptor, &readFdSet))
                     {
                         ingsoc_readMessage(*fileDescriptor, &toRead, hostInfo);
-
+                        /* After sending SYN+ACK and receving the final ack from client
+                         * it will proceed to the next state, which is the final state */
                         if(toRead.ACK == true)
                         {
                             printf("Server - final ACK received\n");
                             state = 2;
                             break;
                         }
+                        /* If for some reason the package is lost or something else is
+                         * received, it will add 1 to a counter and resend the SYN+ACK
+                         * package, after n timeouts it will exit this state */
                         else if(toRead.ACK != true) {
                             printf("Server - ACK not received, attempt: %d", n + 1);
                             n++;
@@ -122,8 +132,12 @@ void Threeway(int *fileDescriptor, fd_set *activeFdSet, struct sockaddr_in *host
                         printf("Timeout\n");
                     }
                 }while(n <= 5);
-                break;
+                break
+
             case 2:
+                /* This is the final state, once we get here the client and server is
+                 * connected and the threeway handshake has been successful, from here
+                 * we will proceed with the sliding window protocol. */
                 printf("Server - Three-way handshake successful\n");
                 running = 0;
                 break;
